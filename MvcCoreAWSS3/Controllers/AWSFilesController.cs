2865cb4b04b4b4cf4b5b3fb313611ba7cc91a218
Carlos00-pixel <58859997+Carlos00-pixel@users.noreply.github.com>
2023-05-18 13:38:59 +0200

    Add project files.

---
+++ MvcCoreAWSS3/Controllers/AWSFilesController.cs
@@ -0,0 +1,74 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSS3.Services;
+
+namespace MvcCoreAWSS3.Controllers
+{
+    public class AWSFilesController : Controller
+    {
+        private ServiceStorageS3 service;
+        private IAmazonS3 clientS3;
+
+        //COMO NUESTRO BUCKET ES PUBLICO, VAMOS A MOSTRAR
+        //LAS IMAGENES EN LA PAGINA INDEX
+        private string BucketUrl;
+        public AWSFilesController
+            (ServiceStorageS3 service, IConfiguration configuration,
+            IAmazonS3 clientS3)
+        {
+            this.BucketUrl = configuration.GetValue<string>("AWS:BucketUrl");
+            this.service = service;
+            this.clientS3 = clientS3;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<string> filesS3 = await this.service.GetVersionsFileAsync();
+            ViewData["BUCKETURL"] = this.BucketUrl;
+            return View(filesS3);
+        }
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            using (Stream stream = file.OpenReadStream())
+            {
+                await this.service.UploadFileAsync(file.FileName, stream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        //SI DESEAMOS VISUALIZAR EL FICHERO POR SI FUERA PRIVADO
+        public async Task<IActionResult> FileAWS(string fileName)
+        {
+            Stream stream = await this.service.GetFileAsync(fileName);
+            return File(stream, "image/png");
+        }
+
+        public async Task<IActionResult> DeleteFile(string fileName)
+        {
+            await this.service.DeleteFileAsync(fileName);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Versiones(string nombreArchivo)
+        {
+            var request = new ListVersionsRequest
+            {
+                BucketName = "bucket-juernes-cma",
+                Prefix = nombreArchivo
+            };
+
+            var response = await this.clientS3.ListVersionsAsync(request);
+
+            return View(response.Versions);
+        }
+
+    }
+}
---
+++ MvcCoreAWSS3/Services/ServiceStorageS3.cs
@@ -0,0 +1,71 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace MvcCoreAWSS3.Services
+{
+    public class ServiceStorageS3
+    {
+        private string BucketName;
+        private IAmazonS3 clientS3;
+
+        public ServiceStorageS3(IConfiguration configuration
+            , IAmazonS3 clientS3)
+        {
+            this.BucketName = configuration.GetValue<string>
+                ("AWS:BucketName");
+            this.clientS3 = clientS3;
+        }
+
+        public async Task<bool>
+            UploadFileAsync(string fileName, Stream stream)
+        {
+            PutObjectRequest request = new PutObjectRequest
+            {
+                InputStream = stream,
+                Key = fileName,
+                BucketName = this.BucketName,
+            };
+
+            PutObjectResponse response = 
+                await clientS3.PutObjectAsync(request);
+            if(response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            DeleteObjectResponse response =
+                await this.clientS3.DeleteObjectAsync(this.BucketName, fileName);
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<string>> GetVersionsFileAsync()
+        {
+            ListVersionsResponse response = 
+                await this.clientS3.ListVersionsAsync(this.BucketName);
+            List<string> versiones =
+                response.Versions.Select(x => x.Key).ToList();
+            return versiones;
+        }
+
+        public async Task<Stream> GetFileAsync(string fileName)
+        {
+            GetObjectResponse response = 
+                await this.clientS3.GetObjectAsync(this.BucketName, fileName);
+            return response.ResponseStream;
+        }
+    }
+}
